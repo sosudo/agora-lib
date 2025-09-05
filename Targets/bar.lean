84c6ce787a25878d28147d9001bc0e4890d7d635
sosudo <shivansh.gour90@gmail.com>
2025-09-05 07:57:30 -0400

    adding template

--- Targets/bar.lean
+++ Targets/bar.lean
@@ -1,4 +1,4 @@
-import Library
+import Library.d
 
 -- Submitted at: 1757067268, Name: bar
 theorem foo2 : x = 5 :=by exact?
--- Targets/foo.lean
+++ Targets/foo.lean
@@ -1,4 +1,4 @@
-import Library
+import Library.d
 
 -- Submitted at: 351413448847/200, Name: foo
-theorem foo : x = 2 := by exact?
+theorem foo : x ≠ 2 := by exact?
