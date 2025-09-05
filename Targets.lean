bd7533c706472cfd693265a651d3ba4f422ec5c5
sosudo <shivansh.gour90@gmail.com>
2025-09-05 06:14:28 -0400

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -1,2 +1,3 @@
 -- Import every target from Agora/targets
 import Targets.foo
+import Targets.bar
---
+++ Targets/bar.lean
@@ -0,0 +1,4 @@
+import Library
+
+-- Submitted at: 1757067268, Name: bar
+theorem foo2 : x = 5 :=by exact?
