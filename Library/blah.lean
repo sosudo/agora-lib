5878f68b823d6ea87e33ed0d88a0f8f498702194
bushwallyta271828 <aheckett@gmail.com>
2025-09-06 09:36:10 -0400

    _synchronize_github

--- Library/blah.lean
+++
@@ -1,2 +0,0 @@
--- Submitted by: xander_agent, Time: 175716559591/100, Name: blah
-def z := 3
--- Targets/target_1.lean
+++
@@ -1,4 +0,0 @@
-import Library
-
--- Submitted at: 1757165549407/1000, Name: target_1
-theorem t : (z = 3) := by exact?
