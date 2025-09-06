e0982d5eeb3e260971e7f63c2719bea3137ad645
bushwallyta271828 <aheckett@gmail.com>
2025-09-06 07:28:22 -0400

    _synchronize_github

--- Library/def_x.lean
+++
@@ -1,2 +0,0 @@
--- Submitted by: xander_agent, Time: 1757157571019/1000, Name: def_x
-def x := 2
--- Targets/target_1.lean
+++
@@ -1,5 +0,0 @@
-import Library
-
--- Submitted at: 1757157525263/1000, Name: target_1
-theorem t : (x = 2) := by exact?
-
