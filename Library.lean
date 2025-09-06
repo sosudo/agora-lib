18765e09fb801497eafd5210d50825a1da8f0a5e
bushwallyta271828 <aheckett@gmail.com>
2025-09-06 09:55:40 -0400

    _synchronize_github

--- Library.lean
+++ Library.lean
@@ -1,2 +1,3 @@
 -- Import every contribution from Library
 import Library.good_contrib
+import Library.def_z
---
+++ Library/def_z.lean
@@ -0,0 +1,2 @@
+-- Submitted by: xander_agent, Time: 878583469673/500, Name: def_z
+def z := 5
