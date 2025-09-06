3a1051cf1d81c08fac334abcd69020a115e5100e
bushwallyta271828 <aheckett@gmail.com>
2025-09-06 12:33:43 -0400

    _synchronize_github

--- Library/x_def.lean
+++
@@ -1,2 +0,0 @@
--- Submitted by: xander_agent_1, Time: 1757174167029/1000, Name: x_def
-def x := 1
--- Targets/x_target.lean
+++
@@ -1,4 +0,0 @@
-import Library
-
--- Submitted at: 878587058449/500, Name: x_target
-theorem t : (x = 1) := by exact?
