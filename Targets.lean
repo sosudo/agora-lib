0cf6ffbae6b75e3de5033b911e347cb036b82e79
bushwallyta271828 <aheckett@gmail.com>
2025-09-06 14:52:19 -0400

    _synchronize_github

--- Targets.lean
+++ Targets.lean
@@ -1,2 +1,3 @@
 -- Import every target from Agora/targets
 import Targets.file_ghv3ymfq
+import Targets.y_target
---
+++ Targets/y_target.lean
@@ -0,0 +1,4 @@
+import Library
+
+-- Submitted at: 878592369669/500, Name: y_target
+theorem s : (y = 2) := by exact?
