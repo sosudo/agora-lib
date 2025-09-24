5b2076be5590c7e12dfee76f4b227cf818b9a908
bushwallyta271828 <aheckett@gmail.com>
2025-09-24 01:50:43 +0000

    Update agora-lib from LeanInterface

--- Targets.lean
+++ Targets.lean
@@ -1,3 +1,4 @@
 -- Import every target from Agora/targets
 import Targets.target
 import Targets.target2
+import Targets.target3
---
+++ Targets/target3.lean
@@ -0,0 +1,4 @@
+import Library
+
+-- Submitted at: 219834830241/125, Name: target3
+theorem t3 : (x3 = 3) := by exact?
